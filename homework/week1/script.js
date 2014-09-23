c561df0f65903abf49474bfcd84eb072f246ac92
Alex Tenn <connection.l10@gmail.com>
2014-09-23 13:43:11 -0400

    Started late but ompleted part 1-2

--- homework/week1/script.js
+++ homework/week1/script.js
@@ -1,20 +1,30 @@
-console.log("javascript working!"); // Just to test. 
 $(document).ready(function() {
-  $.get("http://www.reddit.com/hot.json", function(response) {
-    // If you don't understand line 6, you may want to read up
-    // on Javascript objects, in the slides or internet. 
-    var stories = response.data.children;
-    for(var i in stories) {
-      story = stories[i].data;
-      var elem = $("<li></li>");
-      // Your code here:
-      // In the data in the Javascript object 'story',
-      // find the title, permalink, and upvote count
-      // Then create HTML elements with jQuery (like in line 9)
-      // and append them to the #list element. 
-      $("#list").append(elem);
-      // Look at the JS console in Chrome to see what story looks like
-      console.log(story);
-    }
-  })
-});
+
+    $("#refresh-button").click(function() {
+      $("#list").empty();
+      api();
+      // location.reload(true);
+    });
+
+    function api () {
+
+      $.get("http://www.reddit.com/hot.json", function(response) {
+      var stories = response.data.children;
+      for(var i in stories) {
+        story = stories[i].data;
+        var elem = $("<li></li>");
+        // Your code here:
+        // In the data in the Javascript object 'story',
+        // find the title, permalink, and upvote count
+        // Then create HTML elements with jQuery (like in line 9)
+        // and append them to the #list element. 
+        var title = story.title;
+        var permalink = story.permalink;
+        var upvote_count = story.ups;
+        
+        $("#list").append(elem);
+        $(elem).append(title, $("<br> "), permalink, $("<br>"), upvote_count);
+      }})
+    };
+    api();
+    });
